6e82189013e1e204bd363ba48bfc7d98e9df6bd2
Tom Sanfilippo <sanfilip@gmail.com>
2017-06-14 19:29:43 -0400

    modify temp create sample to use public ids only

--- samples/testcreate.js
+++ samples/testcreate.js
@@ -10,7 +10,11 @@ paperspace.machines.create({
    size: 50,
    billingType: 'hourly',
    machineName: 'My Machine 6',
-   userId: 418,
+   templateId: 'tiddssl', // xe_tempate 45, a public linux template, ml-in-a-box
+   //templateId: 'tinjs77', // template 167, belonging to team 113 with admin 20 (Colin) -- a non-public template
+   networkId: 'ndj60ob', // vlan record 52 (for team 101)
+   teamId: 'te5miig6', // team 101 (AD Test team)
+   userId: 'UIOJ2I5', // user 418, tom@paperspace.io
  }, function(err, resp) {
    if (err) {
      console.log(err);
