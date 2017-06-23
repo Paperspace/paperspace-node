996e9758f42d9b91b16e848e34282abdf0e0aa9c
Tom Sanfilippo <sanfilip@gmail.com>
2017-06-23 01:20:58 -0400

    restore create in bash example

--- samples/testcli.sh
+++ samples/testcli.sh
@@ -16,8 +16,6 @@ node ../bin/paperspace users list
 m_id=`node ../bin/paperspace machines create --machineName "Tom CLI Test 1" --billingType "hourly" \
   --region "East Coast (NY2)" --size 50 --machineType "C1" --templateId "tqalmii" | jq -r '.id'`
 
-#m_id=psp8b3mo
-
 node ../bin/paperspace machines waitfor --machineId "$m_id" --state "ready"
 node ../bin/paperspace machines show --machineId "$m_id"
 
@@ -32,4 +30,4 @@ node ../bin/paperspace accounts script --scriptName "Tom CLI Test 1" \
 node ../bin/paperspace machines restart --machineId "$m_id"
 node ../bin/paperspace machines waitfor --machineId "$m_id" --state "ready"
 
-#node ../bin/paperspace machines destroy --machineId "$m_id"
+node ../bin/paperspace machines destroy --machineId "$m_id"
