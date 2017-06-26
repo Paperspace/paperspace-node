1dc98e11c0e96a8ebceae40f5af255f1e10e98a5
Tom Sanfilippo <sanfilip@gmail.com>
2017-06-26 16:40:20 -0400

    change sample apps to create C1 Ubuntu Server instances

--- samples/sample_node_app.js
+++ samples/sample_node_app.js
@@ -1,6 +1,6 @@
 // Assumes you have installed the paperspace-node package globally.
 // Use "require('../../paperspace-node')" if running from the repository tree.
-var paperspace_node = require('../../paperspace-node');
+var paperspace_node = require('paperspace-node');
 
 // Set apkKey for use of the paperspace api
 var paperspace = paperspace_node({
@@ -18,27 +18,32 @@ paperspace.templates.list(
     }
     console.log(resp.body);
 
-    // Search the response body for a template with the label 'ML in a Box'
+    // Search the response body for a template with the label 'Ubuntu 14.04 Server'
+    // NOTE: to use the 'ML in a Box' template substitute 'ML in a Box' for 'Ubuntu 14.04 Server'
+    // and substitute 'GPU+' or 'P5000' for the machineType value.  The 'C1' machineType is CPU-only.
+    var t_label = 'Ubuntu 14.04 Server'; // or 'ML in a Box'
+    var m_machineType = 'C1'; // or 'GPU+' or 'P5000' if choosing the 'ML in a Box' template
+
     var t_id;
     for(var i = 0; i < resp.body.length; i++) {
-      if(resp.body[i].label === 'ML in a Box') {
+      if(resp.body[i].label === t_label) {
         t_id = resp.body[i].id;
         break;
       }
     }
     if (!t_id) {
-      console.log('Error: \'ML in a Box\' template not found.');
+      console.log('Error: \'' + t_label + '\' template not found.');
       process.exit(1);
     }
 
-    // Create a machine using the found template id
-    console.log('\npaperspace.machines.create({\n  region: \'East Coast (NY2)\',\n  machineType: \'GPU+\',');
+    // Create a machine using the found template id, and the specified machine typ from above
+    console.log('\npaperspace.machines.create({\n  region: \'East Coast (NY2)\',\n  machineType: \'' + m_machineType + '\',');
     console.log('  size: 50,\n  billingType: \'hourly\',\n  machineName: \'Test Machine\',');
     console.log('  templateId: \'' + t_id + '\'}, ...);');
 
     paperspace.machines.create({
         region: 'East Coast (NY2)',
-        machineType: 'GPU+',
+        machineType: m_machineType,
         size: 50,
         billingType: 'hourly',
         machineName: 'Test Machine',
