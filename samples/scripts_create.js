3d33a3dacac9678a9f31ec2b91e56a3f60d569b6
Tom Sanfilippo <sanfilip@gmail.com>
2017-06-29 23:15:56 -0400

    add node sample for scripts create

---
+++ samples/scripts_create.js
@@ -0,0 +1,26 @@
+// Assumes you have installed the paperspace-node package globally.
+// Use "require('../../paperspace-node')" if running from the repository tree.
+var paperspace_node = require('paperspace-node');
+
+// Set apkKey for use of the paperspace api
+var paperspace = paperspace_node({
+  apiKey: '1be4f97...' // Substitue your actual apiKey here
+});
+
+// Retrieve a list of all the templates as a json array
+console.log('\npaperspace.scripts.create(...);');
+
+paperspace.scripts.create({
+    scriptName: 'Test Script',
+    scriptDescription: 'scripts_create.js',
+    machineId: 'ps123abc',
+    scriptFile: 'scripts_create.js',
+    isEnabled: true,
+    runOnce: true
+  }, function(err, resp) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    console.log(resp.body);
+  });
