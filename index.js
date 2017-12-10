a5f8bcea7a111e3f491ccc04b3005486798c2990
Tom Sanfilippo <sanfilip@gmail.com>
2017-12-10 02:36:53 -0500

    detect when running in cli mode

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 'use strict';
 
+// set paperspace_cli to see if we are being run at the command line
+// note: there are four typical entry points to the library: bin/paperspace, lib/paperspace.js, index.js, and lib/cli.js
+if (!global.paperspace_cli) global.paperspace_cli = (require.main === module);
+
 module.exports = require('./lib');
